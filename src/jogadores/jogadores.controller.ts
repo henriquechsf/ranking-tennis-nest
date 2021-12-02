7f34af65c2c8b3df5c7590b7af422176f50585d2
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2021-12-01 21:28:36 -0300

    criando controller jogadores

---
+++ src/jogadores/jogadores.controller.ts
@@ -0,0 +1,10 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CriarJogadorDto } from './dto/criar-jogador.dto';
+
+@Controller('api/v1/jogadores')
+export class JogadoresController {
+  @Post()
+  async criarAtualizarJogador(@Body() criarJogadorDto: CriarJogadorDto) {
+    return { email: criarJogadorDto.email };
+  }
+}
